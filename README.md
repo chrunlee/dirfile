6ee62cee57edf1ba59df51da5dbbdc6ae29efc96
chrunlee <chrunlee@foxmail.com>
2019-06-28 18:35:27 +0800

    dirfile

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # dirfile
 获取文件夹内所有的文件。支持递归获取、异步或同步、过滤、返回信息处理。
+
+# install 
+
+```
+npm install dirfile
+```
+
+# use
+```
+var dirfile = require('dirfile');
+var path = require('path');
+
+//扫描文件夹路径
+var dirPath = 'd:/folder/',
+    //是否异步获取
+    async = true,
+    //是否递归查询子级目录
+    isDeep = true;
+
+
+//# 同步获取文件
+dirfile(dirPath,async,isDeep,function(filePath,stat){
+    return path.extname(filePath) == '.java';
+},function(filePath,stat){
+    return {
+        name : path.basename(filePath),
+        filePath : filePath
+    }
+})
+.then(function(fileList){
+    console.log(fileList);//打印文件列表信息
+})
+.catch(function(err){
+    console.log(err);
+})
+
+
+
+//使用同步获取
+async = false;
+
+var fileList2 = dirfile(dirPath,async,isDeep,function(filePath,stat){
+    return path.extname(filePath) == '.java';
+},function(filePath,stat){
+    return filePath;
+})
+
+console.log(fileList2);//打印输出即可
+
+//简单调用
+dirfile(dirPath)
+.then(function(fileList){
+    console.log(fileList);
+})
+
+```
+
+# 参数
+- isAsync : 是否异步，如果是异步，为true,则返回 `promise` 函数，通过 `then` 调用。如果为false ，则直接同步获取，直接返回获得文件列表。(默认为`true`)
+- isDeep : 是否递归获取子级文件夹,true 则获取所有子级文件夹内符合条件的文件，false 则只获取当前级别下的文件。(默认为`true`)
+- pushFn : 过滤函数，如果没有过滤函数，则获取所有的文件，提供参数`filePath` `stat` . 文件路径以及文件状态信息，需要返回`true/false`. 
+- infoFn : 获取信息函数，如果没有该函数，则默认返回对象. `{filePath : filePath}` 。 可自定义返回值，提供参数`filePath` `stat` . 
+
+参数为向前补充的，第一个参数为`dirPath` 保持不变，如果有两个则第二个参数为 `infoFn` ,如果有三个则`pushFn` `infoFn` ,类似这样。
+
+
+# 说明
+简单做了一个测试，异步要比同步快一倍左右，当然都是小文件，大文件可能差别更多。没有进行细致的测试。
+
+# License 
+MIT
---
+++ index.js
@@ -0,0 +1,131 @@
+/***
+ * 读取文件夹内的文件然后返回文件列表信息
+ * @author chrunlee
+ * 在写一些小工具的时候，经常会用到这种需求，每次情况都大差不差,还要再写一遍，虽然不难..但是很烦!!!
+ ***/
+let path = require('path');
+let fs = require('fs');
+let async = require('async');
+
+function getFileList(dirPath,isDeep,pushFn,infoFn){
+
+    let result = [];
+
+    let files = fs.readdirSync(dirPath);
+
+    files.forEach(function(item){
+
+        var realPath = path.join(dirPath,item);
+
+        var stat = fs.statSync(realPath);
+
+        if(stat.isDirectory() && isDeep){
+
+            result = result.concat(getFileList(realPath,isDeep,pushFn,infoFn));
+
+        }else{
+
+            if(pushFn && pushFn(realPath,stat)){
+
+                result.push(infoFn ? infoFn(realPath,stat) : {filePath : realPath});
+
+            }else if(!pushFn){
+
+                result.push(infoFn ? infoFn(realPath,stat) : {filePath : realPath});
+
+            }
+        }
+    })
+    return result;
+}
+
+function getListAsync(filePath,isDeep,pushFn,infoFn,cb){
+    fs.stat(filePath,(err,stat)=>{
+
+        if(stat.isDirectory() && isDeep){
+            //递归执行。
+            getFileListAsync(filePath,isDeep,pushFn,infoFn)
+            .then(files=>{
+                cb(null,files);
+            })
+            .catch(err=>{
+                cb(err);
+            })
+        }else{
+            var rst = [];
+            if(pushFn && pushFn(filePath,stat)){
+                var info = infoFn ? infoFn(filePath,stat) : {filePath : filePath};
+                rst.push(info);
+            }else if(!pushFn){
+                rst.push(infoFn ? infoFn(realPath,stat) : {filePath : realPath});
+            }
+            cb(null,rst);
+        }
+
+    })
+}
+function getFileListAsync(dirPath,isDeep,pushFn,infoFn){
+    return new Promise((resolve,reject)=>{
+
+        fs.readdir(dirPath,(err,files)=>{
+            if(err){
+                reject(err);
+            }else{
+                async.map(files,(item,cb)=>{
+                    let filePath = path.join(dirPath,item);
+                    getListAsync(filePath,isDeep,pushFn,infoFn,cb);
+                },function(err,values){
+                    if(err){
+                        reject(err);
+                    }else{
+                        //进行平级处理
+                        var result = [];
+                        values.forEach(child=>{
+                            if(child instanceof Array){
+                                result = result.concat(child);
+                            }else{
+                                result.push(child);
+                            }
+                        })
+                        resolve(result);
+                    }
+                });
+            }
+        })
+
+    });
+       
+}
+
+
+module.exports = function(
+    dirPath     //需要查找的文件夹目标路径
+    ,isAsync    //是否同步，如过为true,则为同步获取，如果为false,则为promise 异步获取。
+    ,isDeep     //是否递归查询，如果为true,则查找所有子文件夹，否则只查找当前文件夹下，默认为true
+    ,pushFn     //哪一类的文件可以被查找到，提供一个filePath 和 stats 参数进行判断，需要返回true/false
+    ,infoFn     //都返回文件的哪些信息，默认为filePath .
+    ){
+    //除了第一个参数外，其余都可以忽略，且向后移动
+    if(arguments.length == 1){
+        isAsync = true;
+        isDeep = true;
+        pushFn = null;
+        infoFn = null;
+    }else if(arguments.length == 2){//两个参数，为最后
+        infoFn = arguments[1];
+        isAsync = true;
+        isDeep = true;
+        pushFn = null;
+    }else if(arguments.length == 3){//三个参数，后两个
+        infoFn = arguments[2];
+        pushFn = arguments[1];
+        isAsync = true;
+        isDeep = true;
+    }else if(arguments.length == 4){//四个参数，后三个
+        infoFn = arguments[3];
+        pushFn = arguments[2];
+        isDeep = arguments[1];
+        isAsync = true;
+    }
+    return isAsync ?  getFileListAsync(dirPath,isDeep,pushFn,infoFn) : getFileList(dirPath,isDeep,pushFn,infoFn);
+}
